a3480c8f0be21ccdc8e7e83acb8a42cafcbb6708
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2024-12-07 21:10:33 -0500

    Update db connection

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const browserSync = require("browser-sync").create();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
---
+++ db/pool.js
@@ -0,0 +1,9 @@
+const pool = require("pg");
+
+module.exports = new pool.Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
